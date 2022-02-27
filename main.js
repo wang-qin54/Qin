45bd96753c611730df5aa3a17380bb3b15b51be3
wang-qin54 <54wangqin1995@gmail.com>
2022-02-27 16:19:04 +0800

    add password page unlinked

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let menuIcon = document.getElementById('hamburger-menu-icon');
 let menu = document.getElementById('menu');
 
-const showInfo = () => {
-    menu.style.display = 'block';
+const showInfo = (item) => {
+    item.style.display = 'block';
   }
   
-  menuIcon.addEventListener('click',showInfo);
+  menuIcon.addEventListener('click',showInfo(menu));

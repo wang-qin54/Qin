93d3799c00210f6dfac41b8704e4e4c0f8cf3384
wangqin <wangqin@Qins-Mac-22.local>
2022-03-11 02:09:48 +0800

    add password

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let menuIcon = document.getElementById('hamburger-menu-icon');
 let menu = document.getElementById('menu');
 
+let nextbutton = document.getElementById('submit');
+let password = document.getElementById('password');
+let passwordlayer = document.getElementById('passwordlayer')
+
+// click show hide function
+
 const showInfo = (item) => {
     item.style.display = 'block';
   }
@@ -8,10 +14,18 @@ const showInfo = (item) => {
 const hideInfo = (item) => {
   item.style.display = 'none';
 }
+
+//call
   
   menuIcon.addEventListener('click',function(){
     if(menuIcon.style.display !== 'none'){
       showInfo(menu);
+    } 
+  });
+
+  nextbutton.addEventListener('click',function(){
+    if(password.value == 'qinwang'){
+      hideInfo(passwordlayer);
     } 
   });
 
